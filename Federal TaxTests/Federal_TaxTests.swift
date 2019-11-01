8c4bd13999a4ea571a3cbb2bd2cee5ca1053268b
lcs-JZhang <XZhang@lcs.on.ca>
2019-11-01 10:31:06 -0400

    Initial Commit

---
+++ Federal TaxTests/Federal_TaxTests.swift
@@ -0,0 +1,34 @@
+//
+//  Federal_TaxTests.swift
+//  Federal TaxTests
+//
+//  Created by Jesse Zhang on 2019-11-01.
+//  Copyright © 2019 Jesse Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import Federal_Tax
+
+class Federal_TaxTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
